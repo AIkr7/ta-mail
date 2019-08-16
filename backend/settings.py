f93442e65eec3bfdde0807d99259d40bef2841aa
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2019-08-16 21:44:06 +0700

    fix: settings - env variable not found

--- backend/settings.py
+++ backend/settings.py
@@ -161,7 +161,7 @@ else:
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = env('tamansiswa.indo@gmail.com')
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
 
 # AUTHENTICATION
