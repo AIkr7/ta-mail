26787fdfff1f4fc503cee8ae43bea5d339011ff6
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2019-08-25 19:46:58 +0700

    feat: tutor registration - email html message

--- backend/apps/matchmaking/services.py
+++ backend/apps/matchmaking/services.py
@@ -14,7 +14,8 @@ class MatchmakingNotificationService:
 
     def _get_email_registration(self, answers: list, definition: dict):
         EMAIL_REF = 'email_registration'
-        customer_email_def = self._get_typeform_definition_by_qref(EMAIL_REF, definition.get('fields'))
+        customer_email_def = self._get_typeform_definition_by_qref(
+            EMAIL_REF, definition.get('fields'))
 
         if customer_email_def is None:
             raise ValidationError('No Customer Email Definition Found')
@@ -50,9 +51,28 @@ class TutorRegistrationNotificationService:
     email_manager = GmailManager()
 
     def generate_html_message(self, answers: list):
-        return '<html><p>Mohon menunggu hingga team kami menghubungi anda melalui email ini.</p></html>'
+        return '''<div dir=3D"ltr">Thank you for your registration as a tutor in TamanTutors =
+by TamanSiswa.=C2=A0<div><br><div>We have reviewed your application and it&=
+#39;s been a pleasure to read your ideas about education.</div><div=
+></div><div><br></div><div>We surely can&#39;t wai=
+t to start helping more people to enjoy their learning experience with you.=
+=C2=A0</div><div><br></div><div>Cheers!</div><div><div><div><div>--=C2=A0<b=
+r><div dir=3D"ltr" class=3D"gmail_signature"><div dir=3D"ltr"><div><div dir=
+=3D"ltr"><font color=3D"#6aa84f" size=3D"6" face=3D"tahoma, sans-serif"><b>=
+Ares</b></font><div><font color=3D"#000000" size=3D"4" face=3D"tahoma, sans=
+-serif"><b>CEO &amp; Co-founder, TamanSiswa</b></font></div><div><font colo=
+r=3D"#000000" size=3D"4" face=3D"tahoma, sans-serif"><br></font></div><div>=
+<font color=3D"#000000" face=3D"tahoma, sans-serif">LINE : @tamansiswa</fon=
+t></div><div><font color=3D"#000000" face=3D"tahoma, sans-serif">Instagram =
+: @tamansiswa_</font></div><div><font color=3D"#000000" face=3D"tahoma, san=
+s-serif">Email :=C2=A0<a href=3D"mailto:tamansiswa.indo@gmail.com" target=
+=3D"_blank">tamansiswa.indo@gmail.com</a>=C2=A0</font></div><div><font colo=
+r=3D"#000000" face=3D"tahoma, sans-serif"><br></font></div><div><font color=
+=3D"#000000" face=3D"tahoma, sans-serif">Taman-Siswa.com</font></div><div><=
+font color=3D"#000000" face=3D"tahoma, sans-serif">(Available Soon)</font><=
+/div></div></div></div></div></div></div></div></div></div></div>'''
 
-    def send_form_registration_feedback(self, email_respondent:str, answers: list):
+    def send_form_registration_feedback(self, email_respondent: str, answers: list):
         sender_address = 'tamansiswa.indo'
 
         subject = 'Welcome!'
