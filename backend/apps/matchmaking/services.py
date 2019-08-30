d767451408fe1790119964b73ef6f29c18cfca24
Wisnu Pramadhitya <wisnuprama014@gmail.com>
2019-08-30 23:48:40 +0700

    feat: ares request for tutor message

--- backend/apps/matchmaking/services.py
+++ backend/apps/matchmaking/services.py
@@ -51,28 +51,29 @@ class TutorRegistrationNotificationService:
     email_manager = GmailManager()
 
     def generate_html_message(self, answers: list):
-        return '''<div dir="ltr">Thank you for your registration as a tutor in TamanTutors
-by TamanSiswa.<div><br><div>We are now reviewing your application and it has been a pleasure to read your ideas about education.
-We surely can\'t wait to start helping more people to enjoy their learning experience with you.</div>
-<div></div><div><br></div><div></div><div><br></div><div>Cheers!</div><div><div><div><div><br>
-<div dir="ltr" class="gmail_signature"><div dir="ltr"><div><div dir=
-="ltr"><font color="#6aa84f" size="6" face="tahoma, sans-serif"><b>
-Ares</b></font><div><font color="#000000" size="4" face="tahoma, sans-serif"><b>CEO &amp; Co-founder, TamanSiswa</b>
-</font></div><div><font color="#000000" size="4" face="tahoma, sans-serif"><br></font></div><div>
-<font color="#000000" face="tahoma, sans-serif">LINE : @tamansiswa</font></div><div>
-<font color="#000000" face="tahoma, sans-serif">Instagram: @tamansiswa_</font></div><div>
-<font color="#000000" face="tahoma, sans-serif">Email :<a href="mailto:tamansiswa.indo@gmail.com" target="_blank">tamansiswa.indo@gmail.com</a></font></div>
-<div><font color="#000000" face="tahoma, sans-serif"><br></font></div>
-<div><font color="#000000" face="tahoma, sans-serif">Taman-Siswa.com</font></div><div>
-<font color="#000000" face="tahoma, sans-serif">(Available Soon)</font>
-</div></div></div></div></div></div></div></div></div></div></div>'''
+        return '''<div dir="ltr">Thank you for your registration as a tutor in TamanTutors by TamanSiswa.
+        <div><br><div>We are now reviewing your application and it has been a pleasure for us to read your ideas about education.
+        We surely can't wait to start helping more people to enjoy their learning experience with the most dedicated tutor.</div>
+        <br><div>We'll contact you as soon as possible to inform whether you're selected into interview or not.</div>
+        <div></div><div><br></div><div></div><div><br></div><div>Cheers!</div><div><div><div><div><br>
+        <div dir="ltr" class="gmail_signature"><div dir="ltr"><div><div dir=
+        ="ltr"><font color="#6aa84f" size="6" face="tahoma, sans-serif"><b>
+        Ares</b></font><div><font color="#000000" size="4" face="tahoma, sans-serif"><b>CEO &amp; Co-founder, TamanSiswa</b>
+        </font></div><div><font color="#000000" size="4" face="tahoma, sans-serif"><br></font></div><div>
+        <font color="#000000" face="tahoma, sans-serif">LINE : @tamansiswa</font></div><div>
+        <font color="#000000" face="tahoma, sans-serif">Instagram: @tamansiswa_</font></div><div>
+        <font color="#000000" face="tahoma, sans-serif">Email :<a href="mailto:tamansiswa.indo@gmail.com" target="_blank">tamansiswa.indo@gmail.com</a></font></div>
+        <div><font color="#000000" face="tahoma, sans-serif"><br></font></div>
+        <div><font color="#000000" face="tahoma, sans-serif">Taman-Siswa.com</font></div><div>
+        <font color="#000000" face="tahoma, sans-serif">(Available Soon)</font>
+        </div></div></div></div></div></div></div></div></div></div></div>'''
 
     def send_form_registration_feedback(self, email_respondent: str, answers: list):
         sender_address = 'tamansiswa.indo'
 
-        subject = 'Welcome!'
+        subject = 'Tutor Registration is Being Processed'
         content = self.generate_html_message(answers)
-        bcc = ['chef.riskyaltaresh@gmail.com']
+        bcc = ['moch.riskyaltaresh@gmail.com', 'hnthalia@gmail.com']
         reply_to = ['tamansiswa.indo+helpdesk@gmail.com']
         email = self.email_manager.create_email_messsage(
             subject=subject, body=content, to=[email_respondent], bcc=bcc, reply_to=reply_to)
